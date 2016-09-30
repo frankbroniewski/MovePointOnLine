3fd6ecc33fd56329a3e98f11ba27406872bd8b82
Frank Broniewski <frank.broniewski@gmail.com>
2016-09-30 12:44:46 +0200

    Segmente herausfakturiert

--- PointOnLineIterator.js
+++ PointOnLineIterator.js
@@ -6,52 +6,23 @@ function PointOnLineIterator( way, travelTime ) {
   this.wayDistance = turf.lineDistance( this.way );
   this.accuDistance = 0;
   this.travelTime = travelTime;
-
-  this.currentLineSegment;
-  this.currentLineSegmentIndex = 0;
-  this.segments = [];
-  this.numSegments;
-
   this.fps = 1000 / 60;
+  this.stepLength = this.wayDistance / this.travelTime * this.fps;
 
-  this._setupSegments();
+  console.log( this );
 
 }
 
 
 PointOnLineIterator.prototype.next = function() {
 
-  var segment = this.currentLineSegment,
-      point;
+  var point;
 
-  if ( segment.accuDistance < segment.distance ) {
+  if ( this.accuDistance < this.wayDistance ) {
 
-    return this._getNextPoint( segment );
-
-  } else {
-
-    console.error( segment.accuDistance, segment.distance );
-    throw new Error( 'segment distance overflow' );
-    return;
-
-    //
-    // this.currentLineSegmentIndex++;
-    // this.accuDistance = this.accuDistance + segment.stepLength;
-    //
-    // if ( this.currentLineSegmentIndex < this.numSegments - 1 ) {
-    //
-    //   console.log( 'Changing segment', this.currentLineSegmentIndex );
-    //   this.currentLineSegment = this.segments[ this.currentLineSegmentIndex ];
-    //   console.log(this.currentLineSegment);
-    //
-    //   return this._getNextPoint( this.currentLineSegment );
-    //
-    // } else {
-    //
-    //   console.warn( 'segment index out of range', this );
-    //
-    //   return false
-    // }
+    point = turf.along(this.way.features[ 0 ], this.accuDistance, 'kilometers');
+    this.accuDistance = this.accuDistance + this.stepLength;
+    return point;
 
   }
 
@@ -60,47 +31,6 @@ PointOnLineIterator.prototype.next = function() {
 }
 
 
-PointOnLineIterator.prototype._getNextPoint = function ( segment ) {
-
-  var point = turf.along(segment.segment, segment.accuDistance, 'kilometers');
-  segment.accuDistance = segment.accuDistance + segment.stepLength;
-  this.accuDistance = this.accuDistance + segment.stepLength;
-  return point;
-
-}
-
-
 PointOnLineIterator.prototype.hasNext = function() {
-
   return this.accuDistance < this.wayDistance;
-
-}
-
-
-PointOnLineIterator.prototype._setupSegments = function() {
-
-  var coordinates = this.way.features[ 0 ].geometry.coordinates;
-
-  for ( var i = 0, len = coordinates.length; i < len - 1; i++ ) {
-
-    var segment = turf.lineString([ coordinates[i], coordinates[i + 1] ]),
-        segmentDistance = turf.lineDistance( segment ),
-        segmentRatio = segmentDistance / this.wayDistance,
-        segmentTravelTime = this.travelTime * segmentRatio,
-        stepLength = segmentDistance / segmentTravelTime * this.fps;
-
-    this.segments.push({
-      'segment': segment,
-      'distance': segmentDistance,
-      'ratio': segmentRatio,
-      'travelTime': segmentTravelTime,
-      'stepLength': stepLength,
-      'accuDistance': 0
-    });
-
-  }
-
-  this.currentLineSegment = this.segments[ 0 ];
-  this.numSegments = this.segments.length;
-
 }
